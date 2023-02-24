cd8beea3b6abfe1d433fe4d4f87967e4012e1373
Hex <thehexreader@gmail.com>
2023-02-24 22:58:24 +0300

    making some

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,6 @@
 class UserController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
+  
   def show
     @user = current_user
     @products_sell = current_user.products
